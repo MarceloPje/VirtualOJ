fe97bcdb64e854048cf7e67db41a09ae6c2c12ac
MarceloPje <mar_ce_lo2010@hotmail.com>
2020-11-28 00:32:38 -0300

    activity da central de mandado

---
+++ app/src/main/java/com/example/cardoj/Activity/Centralvirtual.java
@@ -0,0 +1,39 @@
+package com.example.cardoj.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.cardoj.R;
+import com.example.cardoj.central;
+
+public class Centralvirtual extends AppCompatActivity {
+
+
+    private Button buttonDistribuir;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_centralvirtual);
+
+        buttonDistribuir = findViewById(R.id.buttonDistribuir);
+        buttonDistribuir.setOnClickListener(view -> {
+
+            //Chamando a activity do oficial
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+
+            //Passar os dados do mandados
+            intent.putExtra("numero","0800015.75.2020.20.8.5002");
+            intent.putExtra("nome","Carlos Alberto da Silva");
+            intent.putExtra("enderenco","Rua henrique de Ferro");
+            intent.putExtra("data","05/05/2020");
+            intent.putExtra("prazos","15 dias");
+
+            startActivity(intent);
+
+        });
+    }
+}
--- app/src/main/java/com/example/cardoj/Activity/MainActivity.java
+++ app/src/main/java/com/example/cardoj/Activity/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cardoj.R;
 import com.example.cardoj.adapter.PostagemAdapter;
@@ -17,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPostagem;
     private List<Processos> processo = new ArrayList<>();
+    private String numero, nome, enderenco, data, prazos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerPostagem = findViewById(R.id.recyclerPostagem);
+
+        //numero       =  findViewById(R.id.textNumero_processo);
+        //nome         =  findViewById(R.id.textNome_parte);
+        //enderenco    =  findViewById(R.id.textEnderenco_parte);
+        //data         =  findViewById(R.id.textData_intimacao);
+        //prazos       =  findViewById(R.id.textPrazos);
 
         //Define layout
 
@@ -33,10 +45,30 @@ public class MainActivity extends AppCompatActivity {
         this.prepararProcesso();
         PostagemAdapter adapter = new PostagemAdapter(processo);
         recyclerPostagem.setAdapter(adapter);
-    }
+
+        //Recuperar os dados da central
+
+        Bundle dados = getIntent().getExtras();
+        String numero_processo     = dados.getString("numero");
+        String nome_parte_processo = dados.getString("nome");
+        String enderenco_parte_processo = dados.getString("enderenco");
+        String data_parte_processo = dados.getString("data");
+        String prazo_parte_processo = dados.getString("prazos");
+
+        //Configurar valores recuperados
+
+
+        numero    = dados.getString("numero");
+        nome      = dados.getString("nome");
+        enderenco = dados.getString("enderenco");
+        data      = dados.getString("data");
+        prazos    = dados.getString("prazos");
+
+      }
 
     //Criar os mandados
     public void prepararProcesso(){
+
 
         Processos mandado = new Processos("0800001-01.2020.8.20.5001","Maria Ferro Peroz","Rua Paraiso Nº357","05/05/2020","15 dias");
         this.processo.add(mandado);
@@ -62,7 +94,18 @@ public class MainActivity extends AppCompatActivity {
         mandado = new Processos("0800001-08.2020.8.20.5001","Cararina da Costa Lima","Rua Paraiso Nº888","15/10/2020","15 dias");
         this.processo.add(mandado);
 
+        mandado = new Processos("0800001-08.2020.8.20.5001","Cararina da Costa Lima","Rua Paraiso Nº888","15/10/2020","15 dias");
+        this.processo.add(mandado);
 
+
+        mandado = new Processos("0800001-08.2020.8.20.5001","Cararina da Costa Lima","Rua Paraiso Nº888","15/10/2020","15 dias");
+        this.processo.add(mandado);
+
+
+        //adicionando os dados enviado da central
 
     }
-}
+
+
+
+   }
